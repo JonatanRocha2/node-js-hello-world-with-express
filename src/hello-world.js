924c36c0d794e0083e62bb5f0b6b07bc763d9039
Jonatan Ivanhoe da Silva Rocha <jonatanivanhoe@gmail.com>
2024-07-18 13:52:16 -0300

    feat: criado app simples que retorna "Hello World" ao ser acessado pelo navegador

--- src/hello-world.js
+++ src/hello-world.js
@@ -6,26 +6,8 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-// app.listen(port, () => {
-//     console.log(`Server running at http://localhost:${port}`);
-//     process.on('SIGTERM', () => {
-//         app.close(() => {
-//             console.log(`Server terminated.`)
-//         })
-//     })
-// });
-
-// const server = app.listen(3000, () => console.log('Server ready at http://localhost:3000'))
-// process.on('SIGTERM', () => {
-//   server.close(() => {
-//     console.log('Process terminated')
-//   })
-// })
-
 const server = app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
 
-
-// process.kill(process.pid, 'SIGTERM')
 module.exports = { app, server };
